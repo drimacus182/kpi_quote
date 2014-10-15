e69b53f8da0749aa6229078fb0835fcdbad904c2
executeinstaller <drimacus182@gmail.com>
2014-10-15 15:39:36 +0300

    Font size buttons, refactor

--- js/handlers.js
+++ js/handlers.js
@@ -19,7 +19,29 @@ $(document).ready(function () {
     });
     //-----------------------------------
 
+    $('#quote_font_buttons_container').hide();
+    // Show-hide font buttons on hover
+    $('#quote_quote, #quote_font_buttons_container').hover(
+        function () {
+            $('#quote_font_buttons_container').show();
+        },
+        function () {
+            $('#quote_font_buttons_container').hide();
+        });
 
+    var maxQuoteFont = 80;
+    var minQuoteFont = 3;
 
+    $('#quote_font_plus').click(function(){
+        var size = parseInt($('#quote_quote_text').css('font-size'));
+        var newFontSize = Math.min(size + 1, maxQuoteFont);
+        $('#quote_quote_text').css('font-size', newFontSize);
+    });
+
+    $('#quote_font_minus').click(function(){
+        var size = parseInt($('#quote_quote_text').css('font-size'));
+        var newFontSize = Math.max(size - 1, minQuoteFont);
+        $('#quote_quote_text').css('font-size', newFontSize);
+    });
 
 });
